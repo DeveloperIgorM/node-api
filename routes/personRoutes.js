e2c930e08d101500a171fa034c18181b8bce5911
igor matheus <igormatheus090807@gmail.com>
2025-10-27 23:55:05 -0300

    🎉 feat | Implementando rota PATCH, atualização parcial do usuário

--- routes/personRoutes.js
+++ routes/personRoutes.js
@@ -44,8 +44,7 @@ router.get("/", async (req, res) => {
 // Buscando por um usuário especifico de forma dinâmica
 // Extrair o dado da requisição pela URL = req.params
 router.get("/:id", async (req, res) => {
-
-    const id = req.params.id;
+  const id = req.params.id;
 
   try {
     // quero encontrar o usuário que tenha o _id igual o id que venha na requisição req.params
@@ -53,7 +52,7 @@ router.get("/:id", async (req, res) => {
 
     if (!person) {
       res.status(422).json({ message: "O usuário não foi encontrado!" });
-      return 
+      return;
     }
 
     res.status(200).json(person);
@@ -62,4 +61,28 @@ router.get("/:id", async (req, res) => {
   }
 });
 
+// UPDATE - Atualizando dados (PUT, PATCH)
+router.patch("/:id", async (req, res) => {
+  const id = req.params.id;
+  const { name, salary, approved } = req.body;
+
+  const person = {
+    name,
+    salary,
+    approved,
+  };
+
+  try {
+    const updatePerson = await Person.updateOne({ _id: id }, person); // entre chaves -> filtro, segundo parametro é a atualização (person)
+
+    if (updatePerson.matchedCount === 0) {
+      res.status(422).json({ message: "O usuário não foi encontrado!" });
+      return;
+    }
+
+    res.status(200).json(person);
+  } catch (error) {
+    res.status(500).json({ error: error });
+  }
+});
 module.exports = router;
